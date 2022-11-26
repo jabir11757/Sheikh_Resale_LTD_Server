d7eca18631c24223145e3611afc5f404629f0da8
jabir11757 <smjalsaba11757@gmail.com>
2022-11-26 08:41:13 +0600

    Newly added all

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const port = process.env.PORT || 5000;
 
-
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yt0e1fj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const database = client.db('resellerUser');
+        const productsCollection = database.collection('products');
+
+
+        app.get('/products', async (req, res) => {
+
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err))
+
 
 
 app.get('/', (req, res) => {
-    res.send('Resale server running...');
+    res.send('Reseller server is running.......')
 })
 
 app.listen(port, () => {
-    console.log('Resale server is running on port:', port);
+    console.log('Reseller server is running on port:', port)
 })
